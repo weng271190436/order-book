dd59db7131960fb08c12d7e6dae8d57085fdb57e
Wei Weng <wengwei01@gmail.com>
2022-10-05 21:45:36 +0000

    use websocketpp

---
+++ websocketpp.cpp
@@ -0,0 +1,90 @@
+#include <websocketpp/config/asio_client.hpp>
+#include <websocketpp/client.hpp>
+
+#include <iostream>
+
+typedef websocketpp::client<websocketpp::config::asio_tls_client> client;
+typedef websocketpp::lib::shared_ptr<boost::asio::ssl::context> context_ptr;
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+// pull out the type of messages sent by our config
+typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
+
+std::chrono::high_resolution_clock::time_point m_tls_init;
+
+void on_open(client* c, websocketpp::connection_hdl hdl) {
+    std::cout << "on_open" << std::endl;
+    c->send(hdl, "", websocketpp::frame::opcode::text);
+}
+
+void on_message(client* c, websocketpp::connection_hdl hdl, message_ptr msg) {
+    std::cout << "on_message called with hdl: " << hdl.lock().get()
+              << " and message: " << msg->get_payload()
+              << std::endl;
+
+
+    websocketpp::lib::error_code ec;
+    c->send(hdl, msg->get_payload(), msg->get_opcode(), ec);
+    if (ec) {
+        std::cout << "Echo failed because: " << ec.message() << std::endl;
+    }
+}
+
+context_ptr on_tls_init(client *c, websocketpp::connection_hdl hdl) {
+    m_tls_init = std::chrono::high_resolution_clock::now();
+    context_ptr ctx(new boost::asio::ssl::context(boost::asio::ssl::context::tlsv1));
+
+    try {
+        ctx->set_options(boost::asio::ssl::context::default_workarounds |
+                            boost::asio::ssl::context::no_sslv2 |
+                            boost::asio::ssl::context::single_dh_use);
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+    return ctx;
+}
+
+int main(int argc, char* argv[]) {
+    // Create a client endpoint
+    client c;
+    std::string uri = "wss://ws-feed.exchange.coinbase.com:443";
+
+    if (argc == 2) {
+        uri = argv[1];
+    }
+
+    try {
+        // Set logging to be pretty verbose (everything except message payloads)
+        c.set_access_channels(websocketpp::log::alevel::all);
+        c.clear_access_channels(websocketpp::log::alevel::frame_payload);
+
+        // Initialize ASIO
+        c.init_asio();
+
+        // Register our message handler
+        c.set_message_handler(bind(&on_message,&c, ::_1, ::_2));
+        c.set_tls_init_handler(bind(&on_tls_init, &c, ::_1));
+        c.set_open_handler(bind(&on_open, &c, ::_1));
+
+        websocketpp::lib::error_code ec;
+        client::connection_ptr con = c.get_connection(uri, ec);
+        if (ec) {
+            std::cout << "could not create connection because: " << ec.message() << std::endl;
+            return 0;
+        }
+
+        // Note that connect here only requests a connection. No network messages are
+        // exchanged until the event loop starts running in the next line.
+        c.connect(con);
+
+        // Start the ASIO io_service run loop
+        // this will cause a single connection to be made to the server. c.run()
+        // will exit when this connection is closed.
+        c.run();
+    } catch (websocketpp::exception const & e) {
+        std::cout << e.what() << std::endl;
+    }
+}
